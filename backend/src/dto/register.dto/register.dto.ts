bca4c1ba236ccccdd0609a652193e4427be336ab
Tiago Azevedo <tiago.azevedo@olli.vc>
2025-09-01 10:31:52 -0300

    feat: login, register and send message DTO's

---
+++ backend/src/dto/register.dto/register.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class RegisterDto {
+    @IsString()
+    @IsNotEmpty()
+    readonly name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly password: string;
+}
---
+++ backend/src/dto/send-message.dto/send-message.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class SendMessageDto {
+    @IsString()
+    @IsNotEmpty()
+    readonly from: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly to: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly content: string;
+
+    @IsString()
+    readonly timestamp: string;
+}
